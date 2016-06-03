e083ff170ca183a2e7974d60c4063f8326f5f919
choochoopiston <choochoopiston@gmail.com>
2016-06-03 05:16:16 +0000

    デフォルトのルーティングをTOP画面に設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'top/index'
+  root to: "top#index"
 
   resources :blogs
   # The priority is based upon order of creation: first created -> highest priority.
